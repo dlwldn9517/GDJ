fd438b72a6e4e5f772f09afa95d120434da5082d
dlwldn9517 <dlwldn9517@naver.com>
2022-08-02 20:47:17 +0900

    2022-08-02

--- javastudy/Exam02/src/library/Library.java
+++ javastudy/Exam02/src/library/Library.java
@@ -14,7 +14,8 @@ public class Library {
 	}
 	
 	private void addBook() {
-		if(idx > 99) {
+		if(idx == books.length) {
+			System.out.println("더 이상 등록할 수 없습니다.");
 			return;
 		}
 		System.out.println("===== 책 등록 =====");
@@ -24,23 +25,57 @@ public class Library {
 		String author = sc.next();
 		Book book = new Book(idx + 1, title, author);
 		books[idx++] = book;
-		
 	}
 	
 	private void removeBook() {
+		if(idx == 0) {
+			System.out.println("등록된 책이 한 권도 없습니다.");
+			return;
+		}
 		System.out.println("===== 책 삭제 =====");
+		System.out.print("삭제할 책의 번호(1~"+ idx + ") >>> ");
+		int bookNo = sc.nextInt() - 1;
+		if(bookNo < 0 || bookNo >= idx) {
+			System.out.println("책 번호가 " + (bookNo + 1) + "인 책은 없습니다.");
+			return;
+		}
+		System.arraycopy(books, bookNo + 1, books, bookNo, idx - bookNo - 1);
+		books[--idx] = null;
+		System.out.println("책 번호가 " + (bookNo + 1) + "인 책을 삭제했습니다.");
 	}
 	
 	private void findBook() {
+		if(idx == 0) {
+			System.out.println("등록된 책이 한 권도 없습니다.");
+			return;
+		}
 		System.out.println("===== 책 조회 =====");
+		System.out.print("조회할 책 제목 입력 >>> ");
+		String title = sc.next();
+		for(int i = 0; i < idx; i++) {
+			// 저장된 책 제목 : books[i].getTitle();
+			// 조회할 책 제목 : title
+			// String의 동등비교 : equals() 메소드
+			if(books[i].getTitle().equals(title)) {		// if(title..equals(books[i].getTitle())도 가능
+				System.out.println(books[i]);
+				return;	// findBook() 메소드 종료
+			} 
+ 		}
+		System.out.println("제목이 " + title + "인 책은 없습니다.");
 	}
 	
 	private void printAllBooks() {
+		if(idx == 0) {
+			System.out.println("등록된 책이 한 권도 없습니다.");
+			return;
+		}
 		System.out.println("===== 전체 조회 =====");
+		for(int i = 0; i < idx; i++) {
+			System.out.println(books[i]);
+		}
 	}
 	
 	public void manage() {
-
 		while(true) {
 			System.out.print("1.추가 2.삭제 3.조회 4.전체목록 0.프로그램종료 >>> ");
 			int choice = sc.nextInt();
@@ -54,9 +89,6 @@ public class Library {
 					return;	 // manage() 메소드 종료
 			default: System.out.println("알 수 없는 명령입니다. 다시 시도하세요.");
 			}
-			
 		}
-		
 	}
-	
 }
