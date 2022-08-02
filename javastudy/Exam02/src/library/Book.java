1d7e41bb375d86e9ab861757ea7edd5301019874
dlwldn9517 <dlwldn9517@naver.com>
2022-08-02 18:39:05 +0900

    2022-08-02

---
+++ javastudy/Exam02/src/library/Book.java
@@ -0,0 +1,18 @@
+package library;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor	// new Book()
+@AllArgsConstructor	// new Book(1, "어린왕자", "생텍쥐베리")
+@Getter	// getBookNo(), getTitle(), getAuthor()
+@Setter	// setBookNo(1), setTitle("어린왕자"), setAuthor("생텍쥐베리")
+public class Book {
+	private int bookNo;
+	private String title;
+	private String author;
+	
+
+}
---
+++ javastudy/Exam02/src/library/Library.java
@@ -0,0 +1,51 @@
+package library;
+
+import java.util.Scanner;
+
+public class Library {
+
+	private Scanner sc;
+	private Book[] books;
+	
+	public Library() {
+		sc = new Scanner(System.in);
+		books = new Book[100];
+	}
+	
+	private void addBook() {
+		System.out.println("===== 책 등록 =====");
+	}
+	
+	private void removeBook() {
+		System.out.println("===== 책 삭제 =====");
+	}
+	
+	private void findBook() {
+		System.out.println("===== 책 조회 =====");
+	}
+	
+	private void printAllBooks() {
+		System.out.println("===== 전체 조회 =====");
+	}
+	
+	public void manage() {
+
+		while(true) {
+			System.out.print("1.추가 2.삭제 3.조회 4.전체목록 0.프로그램종료 >>> ");
+			int choice = sc.nextInt();
+			sc.nextLine();	// 1(choice)→저장O, Enter(sc.nextLine())→저장X 버리는거
+			switch(choice) {
+			case 1: addBook(); break;
+			case 2: removeBook(); break;
+			case 3: findBook(); break;
+			case 4: printAllBooks(); break;
+			case 0: System.out.println("Library 프로그램을 종료합니다. 감사합니다.");
+					return;	 // manage() 메소드 종료
+			default: System.out.println("알 수 없는 명령입니다. 다시 시도하세요.");
+			}
+			
+		}
+		
+	}
+	
+}
