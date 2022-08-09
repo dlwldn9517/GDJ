f3f7b39a82eca9a9d14b919a831c5172adf88ead
dlwldn9517 <dlwldn9517@naver.com>
2022-08-09 16:26:50 +0900

    2022-08-09

---
+++ javastudy/Practice/src/prac03/BreadAndChange.java
@@ -0,0 +1,28 @@
+package prac03;
+
+public class BreadAndChange {
+	
+	// 필드
+	private int bread;	// 빵
+	private int change;	// 잔돈
+	
+	// 생성자
+	public BreadAndChange(int bread, int change) {
+		this.bread = bread;
+		this.change = change;
+	}
+
+	public int getBread() {
+		return bread;
+	}
+	public void setBread(int bread) {
+		this.bread = bread;
+	}
+	public int getChange() {
+		return change;
+	}
+	public void setChange(int change) {
+		this.change = change;
+	}
+	
+}
