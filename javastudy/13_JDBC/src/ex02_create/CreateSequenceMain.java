f58acc40c491b771daf17fce7deba4dcf38917fe
dlwldn9517 <dlwldn9517@naver.com>
2022-09-06 15:03:45 +0900

    2022-09-06

--- javastudy/13_JDBC/src/ex02_create/CreateSequenceMain.java
+++ javastudy/13_JDBC/src/ex02_create/CreateSequenceMain.java
@@ -12,28 +12,38 @@ public class CreateSequenceMain {
 		Connection con = null;
 		PreparedStatement ps = null;
 		try {
+			// OracleDriver 로드
 			Class.forName("oracle.jdbc.OracleDriver");
-			String url = "jdbc:oracle:thin@localhost:1521:xe";
+			
+			// Connection 생성
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
 			String user = "SCOTT";
 			String password = "TIGER";
-			
 			con = DriverManager.getConnection(url, user, password);
+			
+			// 쿼리문 작성
 			String sql = "CREATE SEQUENCE BOARD_SEQ NOCACHE";
+			
+			// PreparedStatement 객체 생성
 			ps = con.prepareStatement(sql);
+			
+			// 쿼리문 실행
 			ps.execute();
 			
-		} catch (ClassNotFoundException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
+			
+		} finally {
+			// Connection 닫기, PreparedStatement 닫기
+			try {
+				if(ps != null) ps.close();
+				if(con != null) con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
 		}
 		
-		try {
-			if(ps != null) ps.close();
-			if(con != null) con.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
 	}
-
+	
 }
