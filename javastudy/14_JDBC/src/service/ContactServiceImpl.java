8ff07bcf85297961919405b59a88d8c9ef4b012d
dlwldn9517 <dlwldn9517@naver.com>
2022-09-08 10:51:58 +0900

    2022-09-08

---
+++ javastudy/14_JDBC/src/service/ContactServiceImpl.java
@@ -0,0 +1,51 @@
+package service;
+
+import domain.ContactDTO;
+import repository.ContactDAO;
+
+public class ContactServiceImpl implements ContactService {
+	
+	/***************** Field *****************/
+	
+	// DAO에 데이터를 전달하고, DAO로부터 결과를 반환 받기 위해서 DAO를 선언
+	private ContactDAO dao = ContactDAO.getInstance();
+	
+	/***************** Method *****************/
+	
+	
+
+	@Override
+	public void addContact(ContactDTO contact) {
+		int result = dao.insertContact(contact);
+		if(result > 0) {
+			System.out.println("연락처가 등록되었습니다.");
+		} else {
+			System.out.println("연락처 등록이 실패했습니다.");
+		}
+	}
+
+	@Override
+	public void modifyContact(ContactDTO contact) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteContact(int contact_no) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void findContactByNo(int contact_no) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void findAllContacts() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
