b07989406312e1bfedbb0673e503fe84d27262c0
dlwldn9517 <dlwldn9517@naver.com>
2022-10-06 16:06:50 +0900

    2022-10-06

--- jspstudy/01_Servlet/src/main/java/ex02/MyServlet.java
+++ jspstudy/01_Servlet/src/main/java/ex02/MyServlet.java
@@ -1,16 +1,16 @@
 package ex02;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class MyServlet
- */
-@WebServlet("/my")
+
+@WebServlet("/my")	// @WebServlet({"/my", "/me"}) 처럼 2개 이상의 URL Mapping을 지정할 수 있다.
 public class MyServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
@@ -58,6 +58,34 @@ public class MyServlet extends HttpServlet {
 		
 		// 1) MIME-TYPE + 2) 문자셋
 		response.setContentType("text/html; charset=UTF-8");
+		
+		// 3) 응답 스트림 생성
+		// 	  (1) 문자 출력 스트림(*Writer)을 생성
+		//    (2) response 객체로부터 PrintWriter 객체를 얻을 수 있다.
+		//        - IOException 예외 처리가 필요한데 이미 처리되어 있다.
+		//        - write() 메소드보다는 print()/println() 메소드를 사용하는 것을 권장한다.
+		PrintWriter out = response.getWriter();
+		
+		// 4) 응답 만들기 (HTML 문서 만들기)
+		out.println("<html lang=\"ko\">");
+		out.println("<head>");
+		out.println("<meta charset=\"UTF-8\">");
+		out.println("<title>");
+		out.println("나의 첫 번째 응답");
+		out.println("</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>안녕하세요. " + name + "님 반갑습니다 ♥<h1>");
+		if(age >= 20) {
+			out.println("<h1>귀하는 " + age + "살이므로 입장이 가능합니다.<h1>");
+		} else {
+			out.println("<h1>" + age + "살? 애들은 다음에<h1>");
+		}
+		out.println("</body>");
+		out.println("</html>");
+		
+		out.flush();	// 출력 스트림에 남아 있는 모든 데이터 내보내기 (만약을 위해서
+		out.close();
 		
 	}
 
