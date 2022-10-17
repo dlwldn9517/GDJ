1fed4db8183e00395a2e457b7827cfaf6aea2db3
dlwldn9517 <dlwldn9517@naver.com>
2022-10-17 11:52:17 +0900

    2022-10-17

---
+++ jspstudy/03_Mvc/src/main/java/MyService.java
@@ -0,0 +1,7 @@
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface MyService {
+
+	public void execute(HttpServletRequest request, HttpServletResponse response);
+}
--- jspstudy/03_Mvc/src/main/java/NowService.java
+++ jspstudy/03_Mvc/src/main/java/NowService.java
@@ -4,12 +4,13 @@ import java.util.Date;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class NowService {
+public class NowService implements MyService {
 
+	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
 		
 		Date date = new Date();
-		SimpleDateFormat sdf = new SimpleDateFormat("a hh:mm:ss");
+		SimpleDateFormat sdf = new SimpleDateFormat("a h:mm:ss");
 		String now = sdf.format(date);
 		
 		request.setAttribute("result", now);
