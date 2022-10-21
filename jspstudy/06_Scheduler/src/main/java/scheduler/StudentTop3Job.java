1ed310f3c2824e7a0d7f50d045ae8db746e1b5f1
dlwldn9517 <dlwldn9517@naver.com>
2022-10-21 11:43:16 +0900

    2022-10-21

---
+++ jspstudy/06_Scheduler/src/main/java/scheduler/StudentTop3Job.java
@@ -0,0 +1,19 @@
+package scheduler;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class StudentTop3Job implements Job {
+
+	@Override
+	public void execute(JobExecutionContext arg0) throws JobExecutionException {
+		
+		// Job : 스케쥴러가 처리하는 작업
+		
+		
+		
+
+	}
+
+}
