b1f82adf29af1cfced6ca4f781d620e93f466122
dlwldn9517 <dlwldn9517@naver.com>
2022-11-01 18:45:27 +0900

    2022-11-01

--- springstudy/05_Ajax/src/main/java/com/gdu/app05/service/MovieServiceImpl.java
+++ springstudy/05_Ajax/src/main/java/com/gdu/app05/service/MovieServiceImpl.java
@@ -5,7 +5,13 @@ public class MovieServiceImpl implements MovieService {
 	@Override
 	public String getBoxOffice(String targetDt) {
 
-		return null;
+		// API 요청 및 응답
+		
+		String servicekey = "2226b7bc92047543a3afd798f6033fa3";
+		String URL = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json";
+	
+	
+	
 	}
-
+	
 }
