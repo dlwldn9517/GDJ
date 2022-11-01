52c32f19f94dc5684f1332f40a15a3eda71d037b
dlwldn9517 <dlwldn9517@naver.com>
2022-11-01 14:46:31 +0900

    2022-11-01

--- springstudy/05_Ajax/src/main/java/com/gdu/app05/controller/MyController2.java
+++ springstudy/05_Ajax/src/main/java/com/gdu/app05/controller/MyController2.java
@@ -1,7 +1,16 @@
 package com.gdu.app05.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.gdu.app05.domain.Board;
+import com.gdu.app05.service.BoardService;
 
 @Controller
 public class MyController2 {
@@ -15,5 +24,15 @@ public class MyController2 {
 	// BoardServiceImple의
 	// execute1() 메소드 호출
 	
+	@Autowired
+	private BoardService boardService;
+	
+	
+	@ResponseBody
+	@GetMapping(value="board/detail1"
+			  , produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Board> detail1(HttpServletRequest request) {
+		return boardService.execute1(request);
+	}
 
 }
