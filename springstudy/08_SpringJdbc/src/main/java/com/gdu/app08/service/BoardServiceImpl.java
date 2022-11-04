67ea6f8e54d97276cf0e5a8aed897cfa314d08e8
dlwldn9517 <dlwldn9517@naver.com>
2022-11-04 12:27:59 +0900

    2022-11-04

--- springstudy/08_SpringJdbc/src/main/java/com/gdu/app08/service/BoardServiceImpl.java
+++ springstudy/08_SpringJdbc/src/main/java/com/gdu/app08/service/BoardServiceImpl.java
@@ -2,25 +2,17 @@ package com.gdu.app08.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.gdu.app08.domain.BoardDTO;
 import com.gdu.app08.repository.BoardDAO;
 
-
+@Service	// 컴포넌트로 등록
 public class BoardServiceImpl implements BoardService {
 
+	@Autowired
 	private BoardDAO dao;
-	
-	// 생성자의 매개변수 BoardDAO dao로 new BoardDAO()가 주입되고 있다.
-	// BoardAppContext.java 참고
-	public BoardServiceImpl(BoardDAO dao) {	
-		super();
-		this.dao = dao;
-	}
-	
-	// 복습. 이러한 방법도 있다.
-	// AbstractApplicationContext ctx = new AnnotationConfigApplicationContext(BoardAppContext.class);
-	// private BoardDAO dao = ctx.getBean("dao", BoardDAO.class);	// Bean의 이름(id) = 메소드명(ex. dao)
-	
 	
 	@Override
 	public List<BoardDTO> findAllBoards() {
