5c831d31cb06401eab6d2fdf2543945d399a41fb
dlwldn9517 <dlwldn9517@naver.com>
2022-11-08 09:37:02 +0900

    2022-11-08

--- springstudy/09_Mybatis/src/main/java/com/gdu/app09/aop/RequestLoggingAspect.java
+++ springstudy/09_Mybatis/src/main/java/com/gdu/app09/aop/RequestLoggingAspect.java
@@ -1,4 +1,4 @@
-package com.gdu.app08.aop;
+package com.gdu.app09.aop;
 
 import java.util.Map;
 
@@ -23,7 +23,7 @@ public class RequestLoggingAspect {
 		
 	
 	// 포인트컷 설정
-	@Pointcut("within(com.gdu.app08.controller..*)")	// 컨트롤러의 모든 메소드를 포인트컷으로 저장하겠다.
+	@Pointcut("within(com.gdu.app09.controller..*)")	// 컨트롤러의 모든 메소드를 포인트컷으로 저장하겠다.
 														// 컨트롤러의 모든 메소드에서 어드바이스(콘솔에 로그 찍기)가 동작한다.
 	
 	public void setPointCut() { }	// 오직 포인트컷 대상을 결정하기 위한 메소드 (이름:아무거나, 본문:없다)
@@ -34,7 +34,7 @@ public class RequestLoggingAspect {
 		어드바이스 실행 시점
 		@Before, @After, @AfterReturning, @AfterThrowing, @Around
 	*/
-	@Around("com.gdu.app08.aop.RequestLoggingAspect.setPointCut()")		// setPointCut() 메소드에 설정된 포인트컷에서 동작하는 어드바이스
+	@Around("com.gdu.app09.aop.RequestLoggingAspect.setPointCut()")		// setPointCut() 메소드에 설정된 포인트컷에서 동작하는 어드바이스
 	public Object executeLogging(ProceedingJoinPoint joinPoint) throws Throwable {	 // @Around는 반드시 ProceedingJoinPoint joinPoint 선언해야 함
 
 		
