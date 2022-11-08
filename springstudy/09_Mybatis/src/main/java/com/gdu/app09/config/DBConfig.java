eb9f011ab2a212867f4c2a5c3bc79201dcc13a76
dlwldn9517 <dlwldn9517@naver.com>
2022-11-08 10:08:30 +0900

    2022-11-08

--- springstudy/09_Mybatis/src/main/java/com/gdu/app09/config/DBConfig.java
+++ springstudy/09_Mybatis/src/main/java/com/gdu/app09/config/DBConfig.java
@@ -1,63 +1,67 @@
 package com.gdu.app09.config;
 
-import java.util.Collections;
-
-import org.springframework.aop.Advisor;
-import org.springframework.aop.aspectj.AspectJExpressionPointcut;
-import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.interceptor.MatchAlwaysTransactionAttributeSource;
-import org.springframework.transaction.interceptor.RollbackRuleAttribute;
-import org.springframework.transaction.interceptor.RuleBasedTransactionAttribute;
-import org.springframework.transaction.interceptor.TransactionInterceptor;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 /*
-	@EnableTransactionManagement
-	안녕. 난 트랜잭션매니저를 허용하는 애너테이션이야.
+	@PropertySource
+	안녕. 난 프로퍼티 파일을 참조할 수 있는 애너테이션이야.
 */
+@PropertySource(value = {"mybatis/config/db/properties"})
+
 @EnableTransactionManagement
-
-/*
-	@EnableAspectJAutoProxy
-	안녕. 난 Aspect를 자동으로 동작시키는 애너테이션이야.
-*/
 @EnableAspectJAutoProxy
-
 @Configuration
 public class DBConfig {
 
-	// SpringJdbc 처리를 위한 DriverManagerDataSource와 JdbcTemplate을 Bean으로 등록한다.
 	
+	// db.properties 파일을 읽어서 변수에 저장하기
+	// ${프로퍼티명}
+	@Value(value = "${driver}")
+	private String driver;
+	
+	@Value(value = "${url}")
+	private String url;
+	
+	@Value(value = "${username}")
+	private String username;
+	
+	@Value(value = "${password}")
+	private String password;
+	
+	
+	// HikariConfig
 	@Bean
-	public DriverManagerDataSource dataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("oracle.jdbc.OracleDriver");
-		dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
-		dataSource.setUsername("SCOTT");
-		dataSource.setPassword("TIGER");
-		return dataSource;
+	public HikariConfig config() {
+		HikariConfig config = new HikariConfig();
+		config.setDriverClassName(driver);
+		config.setJdbcUrl(url);
+		config.setUsername(username);
+		config.setPassword(password);
+		return config;
 	}
 	
-	@Bean
-	public JdbcTemplate jdbcTemplate() {
-		return new JdbcTemplate(dataSource());  // dataSource() 의존 관계 처리
+	
+	// HikariDataSource
+	@Bean(destroyMethod="close")
+	public HikariDataSource dataSource() {
+		return new HikariDataSource(config());
 	}
 	
 	
-	// 트랜잭션 처리를 위한 TransactionManager를 Bean으로 등록한다.
-	
+	// TransactionManager
 	@Bean
 	public TransactionManager transactionManager() {
 		return new DataSourceTransactionManager(dataSource());
-		
 	}
 	
 }
