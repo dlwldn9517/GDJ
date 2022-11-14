b8013b4ee29fe848388bde91ceef00840e7f8a90
dlwldn9517 <dlwldn9517@naver.com>
2022-11-14 12:51:25 +0900

    2022-11-14

---
+++ springstudy/13_Pjt_User/src/main/java/com/gdu/app13/service/UserService.java
@@ -0,0 +1,12 @@
+package com.gdu.app13.service;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+	
+	public Map<String, Object> isReduceId(String id);
+
+}
---
+++ springstudy/13_Pjt_User/src/main/java/com/gdu/app13/service/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package com.gdu.app13.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gdu.app13.mapper.UserMapper;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserMapper userMapper;
+	
+	@Override
+	public Map<String, Object> isReduceId(String id) {
+		Map<String, Object> result = new HashMap<String, Object>();
+		result.put("isUser", userMapper.selectUserById(id) != null);    // select 결과가 null이 아니면(조회 되었으면) true
+		result.put("isRetireUser", userMapper.selectRetireUserById(id) != null);    // select 결과가 null이 아니면(조회 되었으면) true
+      	return result;
+	}
+
+}
