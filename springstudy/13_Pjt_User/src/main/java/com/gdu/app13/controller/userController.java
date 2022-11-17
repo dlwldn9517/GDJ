b10f49dadcc76208268052a63ac9d090eb0fe05a
dlwldn9517 <dlwldn9517@naver.com>
2022-11-17 17:51:09 +0900

    2022-11-17

--- springstudy/13_Pjt_User/src/main/java/com/gdu/app13/controller/userController.java
+++ springstudy/13_Pjt_User/src/main/java/com/gdu/app13/controller/userController.java
@@ -35,81 +35,79 @@ public class userController {
 	
 	@GetMapping("/user/join/write")
 	public String joinWrite(@RequestParam(required=false) String location
-						  , @RequestParam(required=false) String promotion
-						  , Model model) {
+	                      , @RequestParam(required=false) String promotion
+	                      , Model model) {
 		model.addAttribute("location", location);
 		model.addAttribute("promotion", promotion);
 		return "user/join";
 	}
-	
-	@ResponseBody
+	   
+	@ResponseBody  // ajax할 때 필요
 	@GetMapping(value="/user/checkReduceId", produces=MediaType.APPLICATION_JSON_VALUE)
 	public Map<String, Object> checkReduceId(String id) {
 		return userService.isReduceId(id);
 	}
-	
+	   
 	@ResponseBody
 	@GetMapping(value="/user/checkReduceEmail", produces=MediaType.APPLICATION_JSON_VALUE)
 	public Map<String, Object> checkReduceEmail(String email) {
 		return userService.isReduceEmail(email);
 	}
-	
+	   
 	@ResponseBody
 	@GetMapping(value="/user/sendAuthCode", produces=MediaType.APPLICATION_JSON_VALUE)
 	public Map<String, Object> sendAuthCode(String email) {
 		return userService.sendAuthCode(email);
 	}
-	
+	   
 	@PostMapping("/user/join")
 	public void join(HttpServletRequest request, HttpServletResponse response) {
 		userService.join(request, response);
 	}
-	
-	@GetMapping("/user/retire")
-	public void requiredLogin_retire(HttpServletRequest request, HttpServletResponse response) {
+	   
+	@PostMapping("/user/retire")
+	public void retire(HttpServletRequest request, HttpServletResponse response) {
 		userService.retire(request, response);
 	}
-	
-	@GetMapping("/user/login/form")
+	   
+	@GetMapping("user/login/form")   // <a> 태그를 이용하여 값을 전달하면 GET 방식이다. @GetMapping 
 	public String loginForm(HttpServletRequest request, Model model) {
-		
+	      
 		// 요청 헤더 referer : 이전 페이지의 주소가 저장
-		model.addAttribute("url", request.getHeader("referer"));	// 로그인 후 되돌아 갈 주소 url
-
+		model.addAttribute("url", request.getHeader("referer"));  // 로그인 후 되돌아 갈 주소 url
 		return "user/login";
 	}
-	
+	   
 	@PostMapping("/user/login")
 	public void login(HttpServletRequest request, HttpServletResponse response) {
 		userService.login(request, response);
 	}
-	
+	   
 	@GetMapping("/user/logout")
-	public String logout(HttpServletRequest request, HttpServletResponse response) {
+	public String requiredLogin_logout(HttpServletRequest request, HttpServletResponse response) {
 		userService.logout(request, response);
 		return "redirect:/";
 	}
-	
+	   
 	@GetMapping("/user/check/form")
 	public String requiredLogin_checkForm() {
 		return "user/check";
 	}
-	
+	   
 	@ResponseBody
 	@PostMapping(value="/user/check/pw", produces="application/json")
 	public Map<String, Object> requiredLogin_checkPw(HttpServletRequest request) {
-		return userService.confirmPassword(request);
+	 return userService.confirmPassword(request);
 	}
-	
+	   
 	@GetMapping("/user/mypage")
 	public String requiredLogin_mypage() {
-		return "user/mypage";
+	 return "user/mypage";
 	}
-	
+	   
 	@PostMapping("/user/modify/pw")
 	public void requiredLogin_modifyPw(HttpServletRequest request, HttpServletResponse response) {
 		userService.modifyPassword(request, response);
 	}
-	
-	
+	   
 }
